7b4dcd02bc1f812d89775c12a575070b98410c09
Corey Jackson <cjaxsn@gmail.com>
2026-07-04 19:22:31 +0000

    lighthouse audit

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 import lighthouse from 'lighthouse';
+import * as chromeLauncher from 'chrome-launcher';
 
 console.log("Hello World");
+
+lighthouseAudit("https://jax.tech");
+
+async function lighthouseAudit(strURL)
+{
+	console.log("Starting Lighthouse audit");
+	const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
+	const options = {logLevel: 'info', output: 'html', onlyCategories: ['performance'], port: chrome.port};
+	const runnerResult = await lighthouse(strURL, options);
+
+	// `.report` is the HTML report as a string
+	const reportHtml = runnerResult.report;
+	fs.writeFileSync('lhreport.html', reportHtml);
+
+	// `.lhr` is the Lighthouse Result as a JS object
+	console.log('Report is done for', runnerResult.lhr.finalDisplayedUrl);
+	console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
+
+	chrome.kill();
+}
